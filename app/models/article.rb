12e3465abfb545f5b5bf15efd5a5d4671e2f0d7d
Yuhei Yamakawa <ymkwyh1@gmail.com>
2021-10-12 23:56:21 +0900

    action textの導入

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,6 @@
 # Table name: articles
 #
 #  id         :integer          not null, primary key
-#  content    :text             not null
 #  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -16,16 +15,13 @@
 class Article < ApplicationRecord
 
     has_one_attached :eyecatch
+    has_rich_text :content
 
     validates :title, presence: true
     validates :title, length: {minimum: 2, maximum: 100}
     validates :title, format: { with: /\A(?!\@)/ }
 
     validates :content, presence: true
-    validates :content, length: {minimum: 10}
-    validates :content, uniqueness: true
-
-    validate :validate_title_and_content_length
 
     belongs_to :user
     has_many :comments, dependent: :destroy
@@ -41,15 +37,6 @@ class Article < ApplicationRecord
 
     def like_count
         likes.count
-    end
-    
-    private
-
-    def validate_title_and_content_length
-        char_count = self.title.length + self.content.length
-        unless char_count > 100
-            errors.add(:content, '100文字以上で入力して下さい')
-        end
     end
 
 end
